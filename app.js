a8d02bab32feb4fab1cd0e98eeb07412a5f6f17c
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-17 16:52:19 +0100

    Aggiunto team card

--- app.js
+++ app.js
@@ -2,31 +2,54 @@ const team = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
-      image: 'wayne-barnett-founder-ceo.jpg',
+      image: 'img/wayne-barnett-founder-ceo.jpg',
     },
     {
       name: 'Angela Caroll',
       role: 'Chief Editor',
-      image: 'angela-caroll-chief-editor.jpg',
+      image: 'img/angela-caroll-chief-editor.jpg',
     },
     {
       name: 'Walter Gordon',
       role: 'Office Manager',
-      image: 'walter-gordon-office-manager.jpg',
+      image: 'img/walter-gordon-office-manager.jpg',
     },
     {
       name: 'Angela Lopez',
       role: 'Social Media Manager',
-      image: 'angela-lopez-social-media-manager.jpg',
+      image: 'img/angela-lopez-social-media-manager.jpg',
     },
     {
       name: 'Scott Estrada',
       role: 'Developer',
-      image: 'scott-estrada-developer.jpg',
+      image: 'img/scott-estrada-developer.jpg',
     },
     {
       name: 'Barbara Ramos',
       role: 'Graphic Designer',
-      image: 'barbara-ramos-graphic-designer.jpg',
+      image: 'img/barbara-ramos-graphic-designer.jpg',
     },
 ];
+
+const teamBox = document.querySelector('.team-container');
+
+for( let i = 0; i < team.length; i++ ) {
+
+    const element = team[i];
+    
+    const card = ` <div class="team-card">
+    <div class="card-image">
+      <img
+        src="${element.image}"
+        alt="${element.name}"
+      />
+    </div>
+    <div class="card-text">
+      <h3>${element.name}</h3>
+      <p>${element.role}</p>
+    </div>
+    </div> `;
+
+    teamBox.innerHTML += card;
+
+}
